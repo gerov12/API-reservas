9ddb6f70bcb454bc63ccec925ccc564c4a26a33a
Benjamin Freccero <benfrecce2012@gmail.com>
2022-10-19 19:35:37 -0300

    JWT authorization

--- app/app.py
+++ app/app.py
@@ -1,11 +1,19 @@
-from flask import Flask, jsonify, request
+import json
+from flask import Flask, jsonify, request, make_response
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+import jwt
+import datetime
+from functools import wraps
 
 # configuro app y db
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://grupo41:123456@localhost:3306/apireservas'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "mysql+pymysql://grupo41:123456@localhost:3306/apireservas"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SECRET_KEY"] = "super-secret"
+
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -25,9 +33,11 @@ class Material(db.Model):
         self.producer = producer
         self.delivery_time = delivery_time
 
+
 class MaterialSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'stock', 'producer', 'delivery_time')
+        fields = ("id", "name", "stock", "producer", "delivery_time")
+
 
 # PEDIDO
 class Pedido(db.Model):
@@ -43,44 +53,97 @@ class Pedido(db.Model):
         self.material_id = material_id
         self.quantity = quantity
 
+
 class PedidoSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'user_id', 'colection_id', 'material_id', 'quantity')
+        fields = ("id", "user_id", "colection_id", "material_id", "quantity")
+
+
+# USER
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
 
 # creo las tablas
 with app.app_context():
     db.create_all()
 
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get("token")
+
+        if not token:
+            return jsonify({"message": "El token no existe"}), 403
+        try:
+            data = jwt.decode(token, app.config["SECRET_KEY"])
+        except:
+            return jsonify({"message": "El token ha expirado o es inválido"}), 403
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+@app.route("/login", methods=["GET"])
+def login():
+    username = request.json["username"]
+    password = request.json["password"]
+    user = User.query.filter_by(username=username).first()
+    if user and user.password == password:
+        token = jwt.encode(
+            {
+                "user": username,
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+            },
+            app.config["SECRET_KEY"],
+        )
+        return jsonify({"token": token.decode("UTF-8")})
+    return make_response("Usuario o contraseña incorrectos", 401)
+
+
 # defino las rutas
-@app.route('/materiales', methods=['GET'])
+@app.route("/materiales", methods=["GET"])
+@token_required
 def get_materials():
-    names = request.json['names']
+    names = request.json["names"]
     materials = []
     for name in names:
         m = Material.query.filter_by(name=name).all()
         if m:
             for material in m:
-                material_dump = MaterialSchema.dump(material)
-                materials.append(material_dump)
-    return jsonify(materials)
+                materials.append(material)
+    return jsonify(MaterialSchema(many=True).dump(materials))
 
-@app.route('/reservar_materiales', methods=['PUT'])
+
+@app.route("/reservar_materiales", methods=["PUT"])
+@token_required
 def reserve_materials():
-    reserva = request.json['materials']
-    user_id = request.json['user_id']
-    colection_id = request.json['colection_id']
+    reserva = request.json["materials"]
+    user_id = request.json["user_id"]
+    colection_id = request.json["colection_id"]
     pedidos = []
     for pedido in reserva:
-        material = Material.query.get(pedido['id'])
+        material = Material.query.get(pedido["id"])
         if material:
-            if material.stock >= pedido['quantity']:
-                material.stock = material.stock - pedido['quantity']
-                nuevo_pedido = Pedido(user_id, colection_id, material.id, pedido['quantity'])
+            if material.stock >= pedido["quantity"]:
+                material.stock = material.stock - pedido["quantity"]
+                nuevo_pedido = Pedido(
+                    user_id, colection_id, material.id, pedido["quantity"]
+                )
                 db.session.add(nuevo_pedido)
                 pedidos.append(nuevo_pedido)
     if pedidos:
         db.session.commit()
     return jsonify(PedidoSchema(many=True).dump(pedidos))
+
 
 # corro la app con debug true para que se actualice dinamicamente
 if __name__ == ("__main__"):
